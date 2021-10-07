38526576067d6ddd33bd5e58c232ddd3b0cb8259
subeen-me <subeen.me@gmail.com>
2021-10-07 22:56:04 +0900

    first calendar

--- src/subeen/Calender/Calender.java
+++ src/subeen/Calender/Calender.java
@@ -3,7 +3,11 @@ package subeen.Calender;
 public class Calender {
 
 	public static void main(String[] args) {
-		System.out.println("Hello calendar");
+		System.out.println("Hello calendar"); 
+		System.out.println("일 월 화 수 목 금 토");
+		System.out.println("------------------");
+		System.out.println("1  2  3  4  5  6  7");
+		
 		
 	}
 
