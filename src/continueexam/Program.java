8a73355cd1a0cef8e18322f795100e65508ca160
subeen-me <subeen.me@gmail.com>
2021-10-08 17:24:21 +0900

    continue and break example

---
+++ src/continueexam/Program.java
@@ -0,0 +1,28 @@
+package continueexam;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		int n = 0;
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("값을 sp로 구분해서 5개 이상 입력하세요");
+
+		while (scan.hasNext()) {
+			n = scan.nextInt();
+
+			if (n < 10)  //10보다 작으면 건너뛴다. 
+				continue;
+			
+			if (n > 200)
+				break;
+				
+
+			System.out.println(n);
+		}
+
+	}
+
+}
